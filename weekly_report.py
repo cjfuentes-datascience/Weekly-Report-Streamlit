f3992bba9221973e8b58391562a83af875ee3192
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-04 18:22:28 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -45,7 +45,7 @@ st.subheader('Quotes by Producer:')
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-sns.barplot(x='producer', y='quotes', data=df.head(slider_val), ax=ax1, palette='Blues')
+sns.barplot(x='producer', y='quotes', data=df.head(slider_val), ax=ax1)
 ax1.set_xticklabels(ax1.get_xticklabels(), rotation=90)
 ax1.set_xlabel('PRODUCERS')
 ax1.set_ylabel('# Quotes')
@@ -68,7 +68,7 @@ st.subheader('NB Apps by Producer:')
 
 fig2, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-sns.barplot(x='producer', y='nb_apps', data=df.head(slider_val), ax=ax1, palette='Greens')
+sns.barplot(x='producer', y='nb_apps', data=df.head(slider_val), ax=ax1)
 ax1.set_xticklabels(ax1.get_xticklabels(), rotation=90)
 ax1.set_xlabel('PRODUCERS')
 ax1.set_ylabel('# NB APPS')
@@ -91,7 +91,7 @@ st.pyplot(fig2)
 st.subheader('RW Apps by Producer:')
 
 fig3, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-sns.barplot(x='producer', y='rw_apps', data=df.head(slider_val), ax=ax1, palette='Reds')
+sns.barplot(x='producer', y='rw_apps', data=df.head(slider_val), ax=ax1)
 ax1.set_xticklabels(ax1.get_xticklabels(), rotation=90)
 ax1.set_xlabel('PRODUCERS')
 ax1.set_ylabel('# RW APPS')
