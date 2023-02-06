d6fa66b3c367ff6ff48492d6b2901e23c08bd966
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-06 09:33:31 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -57,13 +57,13 @@ st.write('---')
 col7, col8, col9 = st.columns(3)
 
 with col7:
-    st.metric(label='Total Quotes', value=f"{df['quotes'].head(slider_val).sum()}")
+    st.metric(label=f'Total Quotes Top {slider_val}', value=f"{df['quotes'].head(slider_val).sum()}")
     
 with col8:
-    st.metric(label='Total NB Apps', value=f"{df['nb_apps'].head(slider_val).sum()}")
+    st.metric(label=f'Total NB Apps Top {slider_val}', value=f"{df['nb_apps'].head(slider_val).sum()}")
     
 with col9:
-    st.metric(label='Total RW Apps', value=f"{df['rw_apps'].head(slider_val).sum()}")
+    st.metric(label=f'Total RW Apps Top {slider_val}', value=f"{df['rw_apps'].head(slider_val).sum()}")
 
 st.write('---')
 
