c09de7d6400f8c6c34bc33de70468920fe148f39
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-05 23:57:09 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -41,7 +41,7 @@ with col3:
 st.write('---')
 
 
-col4, col5, col6, col7 = st.columns(4)
+col4, col5, col6 = st.columns(3)
 
 with col4:
     st.metric(label='AVG Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val).mean(), 2)}%")
@@ -51,9 +51,6 @@ with col5:
     
 with col6:
     st.metric(label='AVG RW Apps % Change', value=f"{round(df['pct_change_rw_apps'].head(slider_val).mean(), 2)}%")
-
-with col7:
-    st.metric(label='AVG Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val).mean(), 2)}%")
 
 st.write('---')
 
