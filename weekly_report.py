7e409106ab7bc330ffdf15772360830294714c6c
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-06 16:42:19 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -49,16 +49,17 @@ with col3:
 st.write('---')
 
 
+# this shows the pct change mean based of slider
 col4, col5, col6 = st.columns(3)
 
 with col4:
-    st.metric(label='Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val), 2)}%")
+    st.metric(label='Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val).mean(), 2)}%")
     
 with col5:
-    st.metric(label='NB Apps % Change', value=f"{round(df['pct_change_nb_apps'].head(slider_val), 2)}%")
+    st.metric(label='NB Apps % Change', value=f"{round(df['pct_change_nb_apps'].head(slider_val).mean(), 2)}%")
     
 with col6:
-    st.metric(label='RW Apps % Change', value=f"{round(df['pct_change_rw_apps'].head(slider_val), 2)}%")
+    st.metric(label='RW Apps % Change', value=f"{round(df['pct_change_rw_apps'].head(slider_val).mean(), 2)}%")
 
 # code below matches the one on the actual report
 #with col4:
