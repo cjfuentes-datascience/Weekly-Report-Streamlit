fe49a65ff2d95218bfa954540229100742439c3f
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-06 09:46:22 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -53,14 +53,23 @@ st.write('---')
 
 col4, col5, col6 = st.columns(3)
 
+#with col4:
+#    st.metric(label='AVG Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val).mean(), 2)}%")
+    
+#with col5:
+#    st.metric(label='AVG NB Apps % Change', value=f"{round(df['pct_change_nb_apps'].head(slider_val).mean(), 2)}%")
+    
+#with col6:
+#    st.metric(label='AVG RW Apps % Change', value=f"{round(df['pct_change_rw_apps'].head(slider_val).mean(), 2)}%")
+
 with col4:
-    st.metric(label='AVG Quotes % Change', value=f"{round(df['pct_change_quotes'].head(slider_val).mean(), 2)}%")
+    st.metric(label='Quotes %', value=f"{round((df['quotes'].head(slider_val)/df['quotes'].sum()).mean(), 2)}%")
     
 with col5:
-    st.metric(label='AVG NB Apps % Change', value=f"{round(df['pct_change_nb_apps'].head(slider_val).mean(), 2)}%")
+    st.metric(label='NB Apps %', value=f"{round(df['pct_change_nb_apps'].head(slider_val).mean(), 2)}%")
     
 with col6:
-    st.metric(label='AVG RW Apps % Change', value=f"{round(df['pct_change_rw_apps'].head(slider_val).mean(), 2)}%")
+    st.metric(label='RW Apps %', value=f"{round(df['pct_change_rw_apps'].head(slider_val).mean(), 2)}%")
 
 st.write('---')
 
