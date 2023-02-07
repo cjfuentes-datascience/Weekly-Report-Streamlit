6fde9cc3e2fc141292bc2faff9c5a7385f86cd28
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-06 22:01:23 -0500

    Update weekly_report.py

--- weekly_report.py
+++ weekly_report.py
@@ -94,7 +94,7 @@ st.subheader('To view the report: Select the number of producers using the slide
 slider_val=st.slider(label='', min_value=1, max_value=25, value=5, key=df['producer'])
 
 st.caption(f'Number of Producers: {slider_val}')
-st.subheader(f'Charts will change according to slider value: {slider_val}')
+st.subheader(f'Charts will change according to slider value: {slider_val}:grey_exclamation:')
 st.write('---')
 
 st.subheader('Quotes :rice_ball:')
